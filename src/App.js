aa422b3e9e24b90fa7da827b74fd54b4d5dd7b0d
zahidhasan2806 <jahidh714@gmail.com>
2023-11-03 23:59:01 +0600

    design modified and drag and drop functionality added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Gallery from './Gallery/Gallery';
+import './App.css'
 
 function App() {
   return (
--- src/Gallery/Gallery.js
+++ src/Gallery/Gallery.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './Gallery.css'
 import ImageUpload from '../Image_Upload/ImageUpload';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 const Gallery = () => {
     const [images, setImages] = useState([]);
     const [checkedCount, setCheckedCount] = useState(0);
@@ -48,33 +49,61 @@ const Gallery = () => {
         const newCheckedCount = updatedImages.filter((image) => image.selected).length;
         setCheckedCount(newCheckedCount);
     };
-    return (<div className='container'>
-        <div className="gallery">
+    function handleOnDragEnd(result) {
+        if (!result.destination) return;
+
+        const items = Array.from(images);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
+
+        setImages(items);
+    }
 
 
-
-            <div className="checked-counter"><h4>{checkedCount}  Files selected</h4></div>
-            <button className="delete-button" onClick={() => handleDeleteSelected()}>
-                Delete Selected
-            </button>
-
-            {images?.map((image, index) => (
-                <div
-                    key={image.id}
-                    className={`image-container ${image.selected ? 'selected' : ''} ${index === 0 ? 'featured-image' : ''}`} onClick={() => toggleCheckbox(index)}
-                >
-                    <input type="checkbox" className={`image-checkbox ${image.selected ? 'visible' : ''}`} checked={image.selected} />
-
-                    <img src={image.src} alt="" />
-                </div>
-            ))
-            }
-            <div className="image-container no-hover">
-                <ImageUpload onImageUpload={handleImageUpload} />
+    return (
+        <div className='container'>
+            <div className='gallery-actions'>
+                <div className="checked-counter"><h4>{checkedCount}  Files selected</h4></div>
+                <h5 className="delete-button" onClick={() => handleDeleteSelected()}>
+                    Delete Selected
+                </h5>
             </div>
+            <DragDropContext onDragEnd={handleOnDragEnd}>
+                <Droppable droppableId="images">
+                    {(provided) => (
 
-        </div >
-    </div>
+                        <div className="gallery" {...provided.droppableProps} ref={provided.innerRef}>
+
+
+
+
+
+                            {images?.map((image, index) => {
+                                return (
+                                    <Draggable key={image.id} draggableId={image.id} index={index}>
+                                        {(provided) => (
+                                            <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}
+                                                // key={image.id}
+                                                className={`image-container ${image.selected ? 'selected' : ''} `} onClick={() => toggleCheckbox(index)}
+                                            >
+                                                <input type="checkbox" className={`image-checkbox ${image.selected ? 'visible' : ''}`} checked={image.selected} />
+
+                                                <img src={image.src} alt="" />
+                                            </div>)}
+                                    </Draggable>
+                                );
+                            })}
+                            <div className="image-container no-hover">
+                                <ImageUpload onImageUpload={handleImageUpload} />
+                            </div>
+
+                            {provided.placeholder}
+                        </div >
+                    )}
+                </Droppable>
+            </DragDropContext>
+
+        </div>
     );
 };
 
